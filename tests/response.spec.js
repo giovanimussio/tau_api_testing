106ff53b4523317aae2aede85986d66f8926a166
giovani.mussio <zeuser@ip-192-168-15-3.us-west-2.compute.internal>
2021-07-26 18:46:01 -0300

    Fourth class

---
+++ tests/response.spec.js
@@ -0,0 +1,27 @@
+const app = require('../src/app/app');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+describe('response',()=>{
+    it('json response',() =>{
+        request(app)
+        .get('/course')
+        .end((err,res) => {
+            expect(res.body.id).to.be.equal('1');
+        })
+    });
+    it('text response',() => {
+        request(app)
+        .get('/course')
+        .end((err,res) => {
+            expect(res.text).to.contain('1');
+        })
+    });
+    it('status response', ()=>{
+        request(app)
+        .get('/course')
+        .end((end,res) => {
+            expect(res.ok).to.be.true;
+        })
+    })
+})
